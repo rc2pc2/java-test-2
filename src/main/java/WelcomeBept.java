be844f66284bbc36c3f41989ea5edd324cbf8087
Riccardo Petricca <3359943+rc2pc2@users.noreply.github.com>
2024-06-01 11:12:51 +0200

    added two java example classes

---
+++ src/main/java/WelcomeBept.java
@@ -0,0 +1,10 @@
+
+
+public class WelcomeBept {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.printf("Hello BEPT");
+	}
+
+}
---
+++ src/main/java/org/lessons/java/HelloJava.java
@@ -0,0 +1,10 @@
+package org.lessons.java; // questo e' il package che utilizzero' per questa classe
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.printf("HELLO JAVA");
+	}
+
+}
